13e81d9529c98b52bb91c377f4e1a85eeb957e09
Tomer <tomer@corevo.io>
2018-10-13 10:36:25 +0300

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,14 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const form = document.getElementById("login");
+const err = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //TODO: make sure we don't verify account details client side
+  if (username.value === "tomer" && password.value !== "mom im on tv") {
+    window.location = "wow.html";
+  } else {
+    err.style.display = "block";
+  }
+});
