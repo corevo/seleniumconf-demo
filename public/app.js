1db34439f258f064c65a11123f297883de831470
Tomer <tomer@corevo.io>
2018-10-19 14:35:16 -0500

    fixed my horrible bug, that i should definitely be accountable for

--- public/app.js
+++ public/app.js
@@ -6,7 +6,7 @@ const err = document.getElementById("error");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   //TODO: make sure we don't verify account details client side
-  if (username.value === "tomer" && password.value !== "mom im on tv") {
+  if (username.value === "tomer" && password.value === "mom im on tv") {
     window.location = "wow.html";
   } else {
     err.style.display = "block";
